79f8a915002ca889b3320638e7898db34243dc78
RonnZhuk <name_regex@LAPTOP-ILTTRMJC.localdomain>
2022-11-25 21:42:29 +0500

    Finish change

---
+++ brain_games/scripts/brain_progression.py
@@ -0,0 +1,10 @@
+from brain_games.repeated_code import game_frame
+from brain_games.task import progression_task
+
+
+def main():
+    game_frame(progression_task)
+
+
+if __name__ == '__main__':
+    main()
